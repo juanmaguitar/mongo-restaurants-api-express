e6fc360cb6cbe33520aa13c45fe7cb3a9a3c9a34
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-21 10:44:12 +0100

    TODO & token

--- public/js/controllers/ResultsCtrl.js
+++ public/js/controllers/ResultsCtrl.js
@@ -24,7 +24,4 @@ angular.module('myControllers')
       .then( rests => $rootScope.restaurants = rests )
       .then( MapService.getMarkers )
 
-    const token = 'AIzaSyC-8fnm-fKikIyZvY5Oww9qVdenK_5R3U4';
-    $scope.googleMapsUrl=`https://maps.googleapis.com/maps/api/js?key=${token}`
-
   })
